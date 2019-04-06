4cbc30133234ac4d8fb9221b85f70f69c02956b3
Sheharyarasif <35006380+Sheharyarasif@users.noreply.github.com>
2019-04-06 23:25:43 +0500

    Add files via upload

---
+++ golangexmaples.go
@@ -0,0 +1,32 @@
+package golangexamples
+import (
+"fmt"
+"github.com/ehteshamz/greetings"
+)
+func ConcatSlice string(slicetoconcat []byte) {
+  var s string
+  var b string
+  var del="-"``
+  for i:=0;i<len(slicetocontact);i++{
+    b=string(slicetoconcat[i])
+    s=append(s,b)
+    s=append(s,del)
+  }
+  return s
+}
+func Encrypt(sliceToEncrypt []byte, ceaserCount int) {
+  for i:=0;i<len(sliceToEncrypt);i++{
+    for j:=0;j<ceaserCount;j++{
+      sliceToEncrypt[i++]
+    }
+  }
+
+
+}
+
+func EZGreetings string(name string){
+
+  a=greetings.PrintGreetings(name)
+  prntf(a)
+
+}
